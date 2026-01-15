b1dc8f385ec7c0754408f92deb100cc44f953e64
Mika Ridley <mikaridley26@gmail.com>
2026-01-15 11:15:50 +0200

    Clear code

--- src/index.jsx
+++ src/index.jsx
@@ -1,22 +1,18 @@
 import ReactDOM from 'react-dom/client'
 
 import { BrowserRouter as Router } from 'react-router-dom'
-import { Provider } from 'react-redux'
 
-import * as serviceWorkerRegistration from './serviceWorkerRegistration'
+import * as serviceWorkerRegistration from './services/serviceWorkerRegistration'
 
-import { store } from './store/store'
 import { RootCmp } from './RootCmp'
 
 import './assets/styles/main.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-  <Provider store={store}>
-    <Router>
-      <RootCmp />
-    </Router>
-  </Provider>
+  <Router>
+    <RootCmp />
+  </Router>
 )
 
 serviceWorkerRegistration.register()
