280f4fd619f268cc5666a8f3c1783f96c1c54dad
Mika Ridley <mikaridley26@gmail.com>
2026-01-15 09:17:36 +0200

    Move socials to its own component

--- src/cmps/MainAboutMe.jsx
+++ src/cmps/MainAboutMe.jsx
@@ -1,28 +1,8 @@
 import mikaAvatar from '../assets/imgs/mika-avatar.png'
-import githubIcon from '../assets/imgs/github.svg'
-import linkedinIcon from '../assets/imgs/linkedin.svg'
-import mailIcon from '../assets/imgs/mail.svg'
-import whatsupIcon from '../assets/imgs/whatsup.svg'
-import { showSuccessMsg } from '../services/event-bus.service'
 import resumePdf from '../assets/files/Full Stack Developer - Mika Ridley.pdf'
+import { SocialLinks } from './SocialLinks'
 
 export function MainAboutMe() {
-  const email = 'mika.ridley26@gmail.com'
-  const phoneNumber = '545657297'
-
-  async function handleCopy(ev) {
-    // Stops the email client from opening
-    ev.preventDefault()
-
-    try {
-      await navigator.clipboard.writeText(email)
-      console.log('Email copied to clipboard!')
-      showSuccessMsg('Email had been copied!')
-    } catch (err) {
-      console.error('Unable to copy', err)
-    }
-  }
-
   return (
     <section className="main-about-me">
       <div className="about-me">
@@ -44,38 +24,7 @@ export function MainAboutMe() {
 
       <img className="avatar" src={mikaAvatar} />
 
-      <section className="social-icons">
-        <a
-          className="icon github"
-          href="https://github.com/mikaridley"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <img src={githubIcon} />
-        </a>
-
-        <a
-          className="icon linkedin"
-          href="https://www.linkedin.com/in/mikaridley/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <img src={linkedinIcon} />
-        </a>
-
-        <a className="icon mail" href={`mailto:${email}`} onClick={handleCopy}>
-          <img src={mailIcon} />
-        </a>
-
-        <a
-          className="icon whatsup"
-          href={`https://wa.me/972${phoneNumber}`}
-          target="_blank"
-          rel="noreferrer"
-        >
-          <img src={whatsupIcon} />
-        </a>
-      </section>
+      <SocialLinks />
     </section>
   )
 }
