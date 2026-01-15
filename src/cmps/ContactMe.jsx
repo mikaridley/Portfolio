165a40542fed6d342fdf637e9f24720493dc71c6
Mika Ridley <mikaridley26@gmail.com>
2026-01-15 09:13:36 +0200

    Style and functionality to contact me

--- src/cmps/ContactMe.jsx
+++ src/cmps/ContactMe.jsx
@@ -1,16 +1,46 @@
+import { showSuccessMsg } from '../services/event-bus.service'
+
 export function ContactMe() {
+  //send me email from Formspree
+  async function handleSubmit(ev) {
+    ev.preventDefault()
+
+    const form = ev.target
+    const formData = new FormData(form)
+
+    const response = await fetch('https://formspree.io/f/xlggddyq', {
+      method: 'POST',
+      body: formData,
+      headers: {
+        Accept: 'application/json', // Tells Formspree: "Don't redirect me"
+      },
+    })
+
+    if (response.ok) {
+      form.reset()
+      showSuccessMsg('Message has been sent!')
+    }
+  }
+
   return (
-    <form action="https://formspree.io/f/xlggddyq" method="POST">
+    <form className="contact-me" onSubmit={handleSubmit}>
+      <h2>Send me a message!</h2>
       <label>
-        Your email:
+        Name:
+        <input type="text" name="name" />
+      </label>
+      <label>
+        Email:
         <input type="email" name="email" />
       </label>
       <label>
-        Your message:
+        Message:
         <textarea name="message"></textarea>
       </label>
 
-      <button type="submit">Send</button>
+      <button className="btn submit-btn" type="submit">
+        Send
+      </button>
     </form>
   )
 }
